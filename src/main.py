ff1ba7babfb6c36fa521959b58cfd64c184d7ba0
Ricardo Bánffy <rbanffy@gmail.com>
2010-12-17 21:31:27 -0200

    Added appengine app files

---
+++ src/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# This file is part of "My Own Delicious".
+#
+# "My Own Delicious" is free software: you can redistribute it and/or modify it
+# under the terms of the Affero GNU General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at your option)
+#  any later version.
+#
+# "My Own Delicious" is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+# FITNESS FOR A PARTICULAR PURPOSE.  See the Affero GNU General Public License 
+# for more details.
+#
+# You should have received a copy of the Affero GNU General Public License along 
+# with "My Own Delicious".  If not, see <http://www.gnu.org/licenses/>.
+
+from google.appengine.api import oauth
+from google.appengine.api import urlfetch
+from google.appengine.api import users, xmpp
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+from django.utils import simplejson
+
+import datetime
+import os
+import random
+import logging
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__), 'templates/main.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler)],
+                                         debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
